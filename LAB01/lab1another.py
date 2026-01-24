2974ef55b8ac1fd87ce288595be0bb3a3842f324
sumayaomarr <sumayaomarr@gmail.com>
2026-01-24 10:31:03 -0500

    lab1 test complete

--- LAB01/lab1another.py
+++ LAB01/lab1another.py
@@ -1,32 +1,33 @@
 #  1
 def wins_rock_scissors_paper(player, opponent):
-    p = player.lower()
-    o = opponent.lower()
+    player = player.lower()
+    opponent = opponent.lower()
 
-    if p == o:
+    if player == opponent:
         return False
 
-    if p == "rock" and o == "scissors":
+    if player == "rock" and opponent == "scissors":
         return True
-    if p == "paper" and o == "rock":
+    if player == "paper" and opponent == "rock":
         return True
-    if p == "scissors" and o == "paper":
+    if player == "scissors" and opponent == "paper":
         return True
 
     return False
+
 
 #  2
 def factorial(n):
     result = 1
     i = 1
     while i <= n:
-        result *= i
-        i += 1
+        result = result * i
+        i = i + 1
     return result
 
 
-#  3
-def Fibonacci(n):
+#  3 
+def fibonacci(n):
     if n == 0:
         return 0
     if n == 1:
@@ -39,7 +40,7 @@ def Fibonacci(n):
         c = a + b
         a = b
         b = c
-        i += 1
+        i = i + 1
 
     return b
 
@@ -52,12 +53,12 @@ def sum_to_goal(numbers, goal):
         while j < len(numbers):
             if numbers[i] + numbers[j] == goal:
                 return numbers[i] * numbers[j]
-            j += 1
-        i += 1
+            j = j + 1
+        i = i + 1
     return 0
 
 
-# objects
+# 5
 class UpCounter:
     def __init__(self, stepsize=1):
         self.stepsize = stepsize
@@ -67,15 +68,12 @@ class UpCounter:
         return self.value
 
     def update(self):
-        self.value += self.stepsize
+        self.value = self.value + self.stepsize
 
 
 class DownCounter(UpCounter):
     def __init__(self, stepsize=1):
-        super().__init__(stepsize)
+        UpCounter.__init__(self, stepsize)
 
     def update(self):
-        self.value -= self.stepsize
-
-
-
+        self.value = self.value - self.stepsize
